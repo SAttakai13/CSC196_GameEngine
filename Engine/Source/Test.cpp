35f1cbe3ab6d48df36bfd54eb5e41921fc00b24f
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2024-06-26 10:04:15 -0600

    Add project files.

---
+++ Engine/Source/Test.cpp
@@ -0,0 +1,7 @@
+#include "Test.h"
+#include <iostream>
+
+void Print()
+{
+	std::cout << "Hi there" << std::endl;
+}
---
+++ Game/Source/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../../Engine/Source/Test.h"
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	Print();
+}
